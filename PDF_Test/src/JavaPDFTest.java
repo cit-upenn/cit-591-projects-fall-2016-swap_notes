51b96755f87f3f3288de5dc986611c30cdc27e57
leon-wee <leonwee27@gmail.com>
2016-12-03 15:54:33 -0500

    Implemented search function

--- PDF_Test/src/JavaPDFTest.java
+++ PDF_Test/src/JavaPDFTest.java
@@ -1,12 +1,31 @@
+import java.io.File;
 import java.io.IOException;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.pdfbox.cos.COSDocument;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
 
 public class JavaPDFTest {
 
 	public static void main(String[] args) throws IOException {
-
-		PDFManager pdfManager = new PDFManager();
-		pdfManager.setFilePath("hw2.pdf");
-		System.out.println(pdfManager.ToText());       
+		
+		File pdfFile = new File("bigJava.pdf");
+		PDDocument pdoc = PDDocument.load(pdfFile);
+		System.out.println(pdoc.getNumberOfPages());
+		
+		for(int pageNumber = 1; pageNumber < pdoc.getNumberOfPages(); pageNumber++){
+		    PDFTextStripper s = new PDFTextStripper();
+		    s.setStartPage(pageNumber);
+		    s.setEndPage(pageNumber);
+		    String contents = s.getText(pdoc); 
+		    
+		    if (contents.contains("Array")) {
+		    	int count = StringUtils.countMatches(contents, "Array");
+		    	System.out.println("It works");
+		    	System.out.println("These are the page numbers: " + pageNumber);
+		    	System.out.println("Number of occurrences: " + count);
+		    }
+		}
 
 	}    
 }
