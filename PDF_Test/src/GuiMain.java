a5b15d187cae44fa4d63b16f026f489689a9c17a
Yoon Duk Kim <ydkim@vagvlan531.0224.apn.wlan.wireless-pennnet.upenn.edu>
2016-12-07 18:10:46 -0500

    Added basic GUI class using JavaFX

--- PDF_Test/src/GuiMain.java
+++ PDF_Test/src/GuiMain.java
@@ -1,35 +1,71 @@
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
-public class GuiMain extends Application {
+public class GuiMain extends Application implements EventHandler<ActionEvent>{
 
+	Stage window;
+	Scene scene1;
+	Scene scene2;
+	
+	Button button1;
+	Button button2;
 
-	public class Main {
-
-		Stage window;
-		Button button;
-
-		public static void main(String[] args) {
-			launch(args);
-		}
-
-		@Override
-		public void start(Stage primaryStage) throws Exception {
-			window = primaryStage;
-			window.setTitle("thenewboston - JavaFX");
-			button = new Button("Click me");
-
-			StackPane layout = new StackPane();
-			layout.getChildren().add(button);
-			Scene scene = new Scene(layout, 300, 250);
-
-			window.setScene(scene);
-			window.show();
-		}
-
-
+	public static void main(String[] args) {
+		launch(args);
 	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		
+		window = primaryStage;
+		
+		
+		Label label1 = new Label("Hi, this is Swap Notes!"); 
+		button1 = new Button("Start");
+
+		//change to scene2
+		button1.setOnAction(e -> {
+			window.setScene(scene2);
+			
+		});
+		
+		VBox layout1 = new VBox(20);
+		layout1.getChildren().addAll(label1, button1);
+		
+		scene1 = new Scene(layout1, 400, 300);
+		
+		button2 = new Button("New Button");
+			
+		button2.setOnAction(e -> {
+		
+			System.out.println("New button!!");
+			
+		});
+		
+		StackPane layout2 = new StackPane();
+		layout2.getChildren().add(button2);
+		scene2 = new Scene(layout2, 400, 300);
+		
+		window.setScene(scene1);
+		window.setTitle("Swap Notes");
+		window.show();
+	}
+
+	@Override
+	public void handle(ActionEvent event) {
+		if (event.getSource() == button1) {
+			System.out.println("You just pressed the button.");
+		}
+	}
+
+	
+
 }
+
