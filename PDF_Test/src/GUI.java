3879e7dcaf5c9aa8d01bf326248a5836c616e917
Yoon Duk Kim <ydkim@vagvlan531.0224.apn.wlan.wireless-pennnet.upenn.edu>
2016-12-07 17:07:18 -0500

    added instance variables and methods for GUI class

--- PDF_Test/src/GUI.java
+++ PDF_Test/src/GUI.java
@@ -1,7 +1,38 @@
+import java.util.ArrayList;
 
+/**
+ * This class hangles the user inputs that are passed to other classes
+ * @author ydkim
+ *
+ */
 public class GUI {
 
+	/** 
+	 * The location of the input file 
+	 */
+	private String fileLocation;
 	
+	/**
+	 * The path to save output file
+	 */
+	private String outputLocation;
+	
+	/**
+	 * The list of keywords that the user wants to find
+	 */
+	private ArrayList<String> keywords;
+	
+	/**
+	 * The number of pages user wants as output
+	 */
+	private int pageNumberLimit;
+	
+	/** 
+	 * This number represents the sort mode for the document
+	 * 0: Order by page number (default)
+	 * 1: Order by importance, represented by cosign value of each page
+	 */
+	private int sortByType;
 	
 	
 }
