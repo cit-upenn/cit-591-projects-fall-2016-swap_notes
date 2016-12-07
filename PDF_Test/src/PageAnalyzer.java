227e28130139076f6a35a08940cf753472e462e9
luona9 <luona@seas.upenn.edu>
2016-12-07 18:17:10 -0500

    Implemented FileInputFilter class

--- PDF_Test/src/PageAnalyzer.java
+++ PDF_Test/src/PageAnalyzer.java
@@ -1,6 +1,11 @@
+import java.util.ArrayList;
+
+import org.apache.pdfbox.pdmodel.PDPage;
 
 public class PageAnalyzer {
 
-	
+	public PageAnalyzer(PDPage page, int pageNumber, ArrayList<String> keywords) {
+		System.out.println("pageNumber: " + pageNumber);
+	}
 	
 }
